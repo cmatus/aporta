75dbd1e4b48aaa7a1cd4b54e10010e929053c078
Claudio Matus Alegría <claudio@matus.cl>
2017-03-27 12:12:37 -0300

    Commit masivo

--- gestionaTalento/v.1.0 - MaxPablo/cliente/_js/funciones.js
+++ gestionaTalento/v.1.0 - MaxPablo/cliente/_js/funciones.js
@@ -1,8 +1,53 @@
-$( document ).ready(function() {
-	$("#divMenu").click(function() { 
-		$("nav.desplegable").css("left", ($("nav.desplegable").css("left") == "0px" ? "-150px" : "0px"));
+var urlApi = "http://138.197.7.205/gt/api/web";
+
+$(document).ready(function() {
+
+	$("#divMenu").click(function() {
+		$("nav.desplegable").css("left", ($("nav.desplegable").css("left") == "0px" ? "-200px" : "0px"));
 	});
-	$("#divOpciones").click(function() { 
+	$("#divOpciones").click(function() {
 		$("aside.desplegable").css("height", ($("aside.desplegable").css("height") == "0px" ? "90px" : "0px"));
 	});
+
+	$("#accordion > li").click(function(){
+		if(false == $(this).next().is(':visible')) {
+			$('#accordion > ul').slideUp(300);
+		}
+		$(this).next().slideToggle(300);
+	});
+	$('#accordion > ul').hide();
+	$('#accordion > ul:eq(0)').show();
+
+
 });
+
+/* Funciones Generales */
+
+function ajaxGet(url, callback) {
+	$.get(url).done(function(data){ callback(data); });
+}
+
+function ajaxPost(url, datos, callback) {
+	$.post(url, datos).done(function(data){ callback(data); });
+}
+
+function ajaxPut(url, datos, callback) {
+	$.ajax({
+		url: url,
+		data: datos,
+		type: 'PUT',
+		success: function(response) {
+			//callback(data);
+		}
+	}).done(function(data){ callback(data); });
+}
+
+function ajaxDelete(url, callback) {
+	$.ajax({
+		url: url,
+		type: 'DELETE',
+		success: function(response) {
+			//callback(data);
+		}
+	}).done(function(data){ callback(data); });
+}
